66990c42e33326e61ad83e5b8a3cb49cbb67275f
aaronr12 <ajr8498@g.rit.edu>
2017-12-14 13:14:28 -0500

    changed mad libs page

--- madlibs.js
+++ madlibs.js
@@ -1,58 +1,32 @@
-var questions = 6;
-var questionsLeft = ' [' + questions + ' questions left]';
-var adjective = prompt('Please type an adjective' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var verb = prompt('Please type a verb in past tense' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var city = prompt('please type a city' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var name1 = prompt('please type a first name' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var name2 = prompt('please type another first name' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [ ' + questions + ' questions left]'
-var store = prompt('please type a store name' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var car = prompt('please type a car brand' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var plurNoun = prompt('please type a plural noun' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var adverb = prompt('please type an adverb' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var toy = prompt('please type the name of a toy' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var animal = prompt('please type the name of an animal' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var verb2 = prompt('please type a verb' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var number = prompt('please type a verb' + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left]';
-var adjective2 = prompt('please type an adjective' + questionsLeft);
-alert('All done. Ready for the message?');
-var sentence = "<h2>Once upon a time there were two " + adjective;
-sentence += ' police officers who ' + verb + ' in '+ city + '.';
-sentence += ' There names were ' + name1 + ' and ' + name2 + '.';
-sentence += ' One day, there captain came to them and said, "Someone has stolen from' + store + '."';
-sentence += ' We want you to figure out who did it.'
-sentence += ' As quickly as they could, they got into their ' + car + ' and headed for the store.'
-sentence += ' As luck would have it, the resident expert on ' + plurNoun + ' was already on the scene.'
-sentence += ' "What have you got?" the two men said ' + adverb + '.';
-sentence += ' The man responded by saying, "I think I know who did this." He picked up a small ' + toy + '.'
-sentence += ' This is the calling card of the ' + animal + ' gang. There seems to be fingerprints on it too.'
-sentence += ' "Run the prints, and we will bring in whoever they match to." Says one of the officers.'
-sentence += ' "Lets ' + verb2 + '" Said the other officer. Within ' + number + ' minutes the suspect was in custody.'
-sentence += ' "Another ' + adjective2 + ' day" The officers said in unison.'
-
-document.write(sentence);
+function start(){
+    var adjective = document.getElementById("adjective").value;
+    var verb = document.getElementById("verb").value;
+    var city = document.getElementById("city").value;
+    var name1 = document.getElementById("name1").value;
+    var name2 = document.getElementById("name2").value;
+    var store = document.getElementById("store").value;
+    var car = document.getElementById("car").value;
+    var plurNoun = document.getElementById("plurNoun").value;
+    var adverb = document.getElementById("adverb").value;
+    var toy = document.getElementById("toy").value;
+    var animal = document.getElementById("animal").value;
+    var verb2 = document.getElementById("verb2").value;
+    var number = document.getElementById("number").value;
+    var adjective2 = document.getElementById("adjective2").value;
+    
+    var sentence = "<h2>Once upon a time there were two " + adjective;
+    sentence += ' police officers who ' + verb + ' in '+ city + '.';
+    sentence += ' There names were ' + name1 + ' and ' + name2 + '.';
+    sentence += ' One day, their captain came to them and said, "Someone has stolen from ' + store + '.';
+    sentence += ' We want you to figure out who did it."'
+    sentence += ' As quickly as they could, they got into their ' + car + ' and headed for the store.'
+    sentence += ' As luck would have it, the resident expert on ' + plurNoun + ' was already on the scene.'
+    sentence += ' "What have you got?" the two men said ' + adverb + '.';
+    sentence += ' The man responded by saying, "I think I know who did this." He picked up a small ' + toy + '.'
+    sentence += ' This is the calling card of the ' + animal + ' gang. There seem to be fingerprints on it too.'
+    sentence += ' "Run the prints, and we will bring in whoever they match to." Says one of the officers.'
+    sentence += ' "We should' + verb2 + '" Said the other officer. Within ' + number + ' minutes the suspect was in custody.'
+    sentence += ' "Another ' + adjective2 + ' day" The officers said in unison.'
+    document.write(sentence);
+    document.write("<br>Reload Page to Start Again");
+}
